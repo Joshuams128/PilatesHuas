69cfce7d66188be7c4281b5d5c201e6b7031b8f2
Joshua <145717861+Joshuams128@users.noreply.github.com>
2026-03-04 20:58:49 +0000

    updated site + speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Pilates Haus - Reformer Pilates Studio in Pickering, Ontario",
@@ -67,6 +69,8 @@ export default function RootLayout({
         <Navbar />
         <main>{children}</main>
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
